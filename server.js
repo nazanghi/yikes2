b3bc785b5c395b6ec0be6fc6e77b10f66e23752b
Nico Zanghi <nico.zang7@gmail.com>
2021-01-11 23:27:50 -0500

    stage for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const path = require('path')
+const helmet = require('helmet')
 
 const AppRouter = require("./routes/AppRouter")
 
@@ -14,6 +15,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(helmet({contentSecurityPolicy: false}))
 
 app.get('/', (request, response) => response.send({message: "It's a-me, a-server!"}))
 app.use('/api', AppRouter) 
